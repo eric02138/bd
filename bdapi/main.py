d6ca9dfdcc821085c5d53c6e35e27830bcfc6254
eric02138 <emattison@gmail.com>
2025-12-16 14:21:22 -0500

    ran black

--- bdapi/main.py
+++ bdapi/main.py
@@ -21,30 +21,32 @@ DATABASE_NAME = os.getenv("DATABASE_NAME", "bdapi")
 db_client: Optional[AsyncIOMotorClient] = None
 database = None
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
     global db_client, database
-    
+
     print("Connecting to MongoDB...")
     db_client = AsyncIOMotorClient(DATABASE_URL)
     database = db_client[DATABASE_NAME]
-    
+
     # Test the connection
     try:
-        await db_client.admin.command('ping')
+        await db_client.admin.command("ping")
         print("Successfully connected to MongoDB!")
     except Exception as e:
         print(f"Error connecting to MongoDB: {e}")
         raise
-    
+
     yield
-    
+
     # Shutdown
     print("Closing MongoDB connection...")
     if db_client:
         db_client.close()
     print("MongoDB connection closed.")
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -53,36 +55,46 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
+
 async def get_database():
     return database
+
 
 class BDUser(BaseModel):
     username: str
     password: str
     robots: Optional[List[str]] = []
 
+
 class RobotEvent(BaseModel):
     robot: Optional[str] = None
-    status: Optional[str] = None  # could be an enum, and could even be its own collection
+    status: Optional[str] = (
+        None  # could be an enum, and could even be its own collection
+    )
     description: Optional[str] = None
     createdAt: Optional[datetime] = None
+
 
 # Empty for now... the root route could point to the docs, and maybe the version route
 # could point to a change doc.
 @app.get("/")
 async def get_root():
-    return {"Nothing": "Here"}
+    return {"API_Name": "RobotAPI"}
+
 
 @app.get("/v1")
 async def get_version():
-    return {"Nothing": "Here"}
+    return {"Version": "1.0"}
+
 
 # Note: This isn't how to do auth.  We'd use OAuth or something like that.
 # This endpoint is more geared to see which user owns which robots, for filtering in the future.
@@ -93,14 +105,17 @@ async def post_bduser(data: BDUser):
         all_users = []
 
         async for bduser in db.bduser.find():
-            all_users.append({
-                "username": bduser["username"],
-                "password": bduser["password"],
-                "robots": bduser["robots"]
-            })
+            all_users.append(
+                {
+                    "username": bduser["username"],
+                    "password": bduser["password"],
+                    "robots": bduser["robots"],
+                }
+            )
         return all_users
-    single_user = await db.bduser.find_one({"username": data.username}, {'_id': 0})
+    single_user = await db.bduser.find_one({"username": data.username}, {"_id": 0})
     return single_user
+
 
 # FastAPI's route matching is dumb.
 @app.get("/v1/robot_event_by_username/{username}")
@@ -110,14 +125,17 @@ async def get_robot_event_by_username(username: str):
     bduser_robots = bduser_data["robots"]
     robot_events = []
     async for robot_event in db.robot_event.find({"robot": {"$in": bduser_robots}}):
-        robot_events.append({
-            "_id": str(robot_event["_id"]),
-            "robot": robot_event["robot"],
-            "status": robot_event["status"],
-            "description": robot_event["description"],
-            "createdAt": robot_event["createdAt"]
-        })
+        robot_events.append(
+            {
+                "_id": str(robot_event["_id"]),
+                "robot": robot_event["robot"],
+                "status": robot_event["status"],
+                "description": robot_event["description"],
+                "createdAt": robot_event["createdAt"],
+            }
+        )
     return robot_events
+
 
 # A bit redundant, but could be useful for sharing what's going on with a single robot
 @app.get("/v1/robot_event/{robot}")
@@ -125,24 +143,27 @@ async def get_robot_event_by_robot(robot: str):
     db = await get_database()
     robot_events = []
     async for robot_event in db.robot_event.find({"robot": robot}):
-        robot_events.append({
-            "_id": str(robot_event["_id"]),
-            "robot": robot_event["robot"],
-            "status": robot_event["status"],
-            "description": robot_event["description"],
-            "createdAt": robot_event["createdAt"]
-        })
+        robot_events.append(
+            {
+                "_id": str(robot_event["_id"]),
+                "robot": robot_event["robot"],
+                "status": robot_event["status"],
+                "description": robot_event["description"],
+                "createdAt": robot_event["createdAt"],
+            }
+        )
     return robot_events
+
 
 # This endpoint allows optional parameters for filtering, which get passed along to the DB
 @app.get("/v1/robot_event")
 async def get_robot_event(
-        robot: Optional[str] = None,
-        status: Optional[str] = None,
-        description: Optional[str] = None,
-        dateFrom: Optional[str] = None,
-        dateTo: Optional[str] = None
-    ):
+    robot: Optional[str] = None,
+    status: Optional[str] = None,
+    description: Optional[str] = None,
+    dateFrom: Optional[str] = None,
+    dateTo: Optional[str] = None,
+):
     filters = {}
     if robot:
         filters.update({"robot": robot})
@@ -157,14 +178,17 @@ async def get_robot_event(
     db = await get_database()
     all_robot_events = []
     async for robot_event in db.robot_event.find(filters):
-        all_robot_events.append({
-            "_id": str(robot_event["_id"]),
-            "robot": robot_event["robot"],
-            "status": robot_event["status"],
-            "description": robot_event["description"],
-            "createdAt": robot_event["createdAt"]
-        })
+        all_robot_events.append(
+            {
+                "_id": str(robot_event["_id"]),
+                "robot": robot_event["robot"],
+                "status": robot_event["status"],
+                "description": robot_event["description"],
+                "createdAt": robot_event["createdAt"],
+            }
+        )
     return all_robot_events
+
 
 @app.get("/v1/export_robot_event_by_username/{username}")
 async def get_export_robot_event_by_username(
@@ -173,22 +197,20 @@ async def get_export_robot_event_by_username(
     description: Optional[str] = None,
     dateFrom: Optional[str] = None,
     dateTo: Optional[str] = None,
-    format: Optional[str] = "csv"):
+    format: Optional[str] = "csv",
+):
 
     if format not in ["json", "csv"]:
         raise HTTPException(status_code=400, detail="Format must be 'json' or 'csv'")
 
     db = await get_database()
     bduser_data = await db.bduser.find_one({"username": username})
-    
+
     if not bduser_data:
-        return Response(
-            content="User not found",
-            status_code=404
-        )
-    
+        return Response(content="User not found", status_code=404)
+
     bduser_robots = bduser_data["robots"]
-    
+
     filters = {}
     if status:
         filters.update({"status": status})
@@ -199,7 +221,9 @@ async def get_export_robot_event_by_username(
             date_from_str = datetime.fromisoformat(dateFrom)
             filters.update({"createdAt": {"$gte": date_from_str}})
         except ValueError:
-            raise HTTPException(status_code=400, detail="Invalid dateFrom format. Use YYYY-MM-DD")
+            raise HTTPException(
+                status_code=400, detail="Invalid dateFrom format. Use YYYY-MM-DD"
+            )
     if dateTo:
         try:
             date_from_str = datetime.fromisoformat(dateTo)
@@ -209,16 +233,20 @@ async def get_export_robot_event_by_username(
             else:
                 filters["createdAt"]["$lt"] = date_from_str
         except ValueError:
-            raise HTTPException(status_code=400, detail="Invalid dateTo format. Use YYYY-MM-DD")
+            raise HTTPException(
+                status_code=400, detail="Invalid dateTo format. Use YYYY-MM-DD"
+            )
     db = await get_database()
     all_robot_events = []
     async for robot_event in db.robot_event.find(filters):
-        all_robot_events.append({
-            "robot": robot_event["robot"],
-            "status": robot_event["status"],
-            "description": robot_event["description"],
-            "createdAt": robot_event["createdAt"]
-        })
+        all_robot_events.append(
+            {
+                "robot": robot_event["robot"],
+                "status": robot_event["status"],
+                "description": robot_event["description"],
+                "createdAt": robot_event["createdAt"],
+            }
+        )
     # JSON
     if format == "json":
         return all_robot_events
@@ -227,29 +255,34 @@ async def get_export_robot_event_by_username(
     writer = csv.writer(output)
     writer.writerow(["Robot", "Status", "Description", "Created At"])
     async for robot_event in db.robot_event.find({"robot": {"$in": bduser_robots}}):
-        writer.writerow([
-            robot_event["robot"],
-            robot_event["status"],
-            robot_event["description"],
-            robot_event["createdAt"]
-        ])
+        writer.writerow(
+            [
+                robot_event["robot"],
+                robot_event["status"],
+                robot_event["description"],
+                robot_event["createdAt"],
+            ]
+        )
     csv_content = output.getvalue()
     output.close()
     return Response(
         content=csv_content,
         media_type="text/csv",
-        headers={"Content-Disposition": f"attachment; filename=robot_events_{username}.csv"}
+        headers={
+            "Content-Disposition": f"attachment; filename=robot_events_{username}.csv"
+        },
     )
+
 
 @app.get("/v1/export_robot_event")
 async def get_export_robot_event(
-        robot: Optional[str] = None,
-        status: Optional[str] = None,
-        description: Optional[str] = None,
-        dateFrom: Optional[str] = None,
-        dateTo: Optional[str] = None,
-        format: Optional[str] = "csv"
-    ):
+    robot: Optional[str] = None,
+    status: Optional[str] = None,
+    description: Optional[str] = None,
+    dateFrom: Optional[str] = None,
+    dateTo: Optional[str] = None,
+    format: Optional[str] = "csv",
+):
 
     if format not in ["json", "csv"]:
         raise HTTPException(status_code=400, detail="Format must be 'json' or 'csv'")
@@ -266,7 +299,9 @@ async def get_export_robot_event(
             date_from_str = datetime.fromisoformat(dateFrom)
             filters.update({"createdAt": {"$gte": date_from_str}})
         except ValueError:
-            raise HTTPException(status_code=400, detail="Invalid dateFrom format. Use YYYY-MM-DD")
+            raise HTTPException(
+                status_code=400, detail="Invalid dateFrom format. Use YYYY-MM-DD"
+            )
     if dateTo:
         try:
             date_from_str = datetime.fromisoformat(dateTo)
@@ -276,16 +311,20 @@ async def get_export_robot_event(
             else:
                 filters["createdAt"]["$lt"] = date_from_str
         except ValueError:
-            raise HTTPException(status_code=400, detail="Invalid dateTo format. Use YYYY-MM-DD")
+            raise HTTPException(
+                status_code=400, detail="Invalid dateTo format. Use YYYY-MM-DD"
+            )
     db = await get_database()
     all_robot_events = []
     async for robot_event in db.robot_event.find(filters):
-        all_robot_events.append({
-            "robot": robot_event["robot"],
-            "status": robot_event["status"],
-            "description": robot_event["description"],
-            "createdAt": robot_event["createdAt"]
-        })
+        all_robot_events.append(
+            {
+                "robot": robot_event["robot"],
+                "status": robot_event["status"],
+                "description": robot_event["description"],
+                "createdAt": robot_event["createdAt"],
+            }
+        )
     # JSON
     if format == "json":
         return all_robot_events
@@ -294,16 +333,18 @@ async def get_export_robot_event(
     writer = csv.writer(output)
     writer.writerow(["Robot", "Status", "Description", "Created At"])
     for robot_event in all_robot_events:
-        writer.writerow([
-            robot_event["robot"],
-            robot_event["status"],
-            robot_event["description"],
-            robot_event["createdAt"]
-        ])
+        writer.writerow(
+            [
+                robot_event["robot"],
+                robot_event["status"],
+                robot_event["description"],
+                robot_event["createdAt"],
+            ]
+        )
     csv_content = output.getvalue()
     output.close()
     return Response(
         content=csv_content,
         media_type="text/csv",
-        headers={"Content-Disposition": "attachment; filename=robot_events.csv"}
+        headers={"Content-Disposition": "attachment; filename=robot_events.csv"},
     )
--- bdapi/test_endpoint.py
+++ bdapi/test_endpoint.py
@@ -1,7 +1,6 @@
 import requests
 
-r = requests.get('http://127.0.0.1:8000/v1/username')
+r = requests.get("http://127.0.0.1:8000/v1/username")
 
 
 print(r.__dict__)
-
